d98e65286b05877162d973deec49ed4b8a8fe5a0
czaro79 <drocez@gmail.com>
2021-07-02 20:35:27 +0200

    Commit 2 - Part 1 finished

--- models/Item.java
+++ models/Item.java
@@ -1,6 +1,48 @@
 package models;
 
 public class Item {
+    private String name;
+    private double price;
 
-  
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(Item source) {
+        this.name = source.name;
+        this.price = source.price;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return name + ": $" + price + " ";
+    }
 }
